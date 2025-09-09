ac526bbd142735a4bc68f308ce611fc11fee2493
Pierre Paulo <pierresantista@hotmail.com>
2025-09-09 14:24:55 -0300

    feat(menu): add product store and connect PizzaList

--- src/components/home/pizza-list.tsx
+++ src/components/home/pizza-list.tsx
@@ -2,12 +2,16 @@
 
 import { Product } from "@/generated/prisma";
 import { PizzaItem } from "./pizza-item";
+import { useProductsStore } from "@/stores/products";
+import { useEffect } from "react";
 
 type Props = {
   pizzas: Product[];
 };
 
 export const PizzaList = ({ pizzas }: Props) => {
+  const products = useProductsStore();
+  useEffect(() => products.setProducts(pizzas), []);
   return (
     <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
       {pizzas.map((item: Product) => (
