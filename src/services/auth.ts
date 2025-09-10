a2892b9545305cf8bdc503ca938ebfaf75b1ed10
Pierre Paulo <pierresantista@hotmail.com>
2025-09-10 09:38:39 -0300

    feat(api): create service layer and add bcrypt/uuid dependencies

---
+++ src/services/auth.ts
@@ -0,0 +1,48 @@
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+import { v4 } from "uuid";
+
+export const hasEmail = async (email: string) => {
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+  return user ? true : false;
+};
+export const validadeAuth = async (email: string, password: string) => {
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+  if (!user) return false;
+  if (!bcrypt.compareSync(password, user.password)) return false;
+  return {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  };
+};
+export const createuser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email: email.toLowerCase(),
+        password: bcrypt.hashSync(password, 8),
+      },
+    });
+    return { id: user.id, name: user.name, email: user.email };
+  } catch (e) {
+    return null;
+  }
+};
+export const createUserToken = async (userId: number) => {
+  const token = v4();
+  await prisma.user.update({
+    where: { id: userId },
+    data: { token },
+  });
+  return token;
+};
