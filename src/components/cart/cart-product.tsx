b843a65cb227b990781f165ef086283c3c9e0327
Pierre Paulo <pierresantista@hotmail.com>
2025-09-10 09:00:01 -0300

    feat(cart): add increment and decrement item quantity actions

--- src/components/cart/cart-product.tsx
+++ src/components/cart/cart-product.tsx
@@ -6,6 +6,7 @@ import { CartItem } from "@/types/cart-item";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { useState } from "react";
+import { useCart } from "@/stores/cart";
 
 type Props = {
   data: CartItem;
@@ -14,9 +15,25 @@ type Props = {
 export const CartProduct = ({ data }: Props) => {
   const [qt, setQt] = useState(data.quantity);
 
+  const cart = useCart();
+
   const products = useProductsStore();
   let product = products.products.find((item) => item.id === data.productId);
   if (!product) return null;
+
+  const handleMinusClick = () => {
+    if (qt - 1 <= 0) {
+      cart.removeItem(data.productId);
+    } else {
+      cart.addItem({ productId: data.productId, quantity: -1 });
+      setQt(qt - 1);
+    }
+  };
+
+  const handlePlusClick = () => {
+    cart.addItem({ productId: data.productId, quantity: 1 });
+    setQt(qt + 1);
+  };
 
   return (
     <div className="flex items-center gap-3">
@@ -34,11 +51,11 @@ export const CartProduct = ({ data }: Props) => {
         <div className="text-sm">{decimalToMoney(product.price)} </div>
       </div>
       <div className="flex items-center bg-secondary p-2 rounded-md">
-        <Button size="sm" variant="ghost">
+        <Button size="sm" variant="ghost" onClick={handleMinusClick}>
           -
         </Button>
         <div className="mx-3">{qt} </div>
-        <Button size="sm" variant="ghost">
+        <Button size="sm" variant="ghost" onClick={handlePlusClick}>
           +
         </Button>
       </div>
